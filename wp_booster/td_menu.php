679e0f23c1e8e518e532076df7839e0496baf9c7
tagdiv <contact@email.ro>
2015-03-24 08:46:50 +0200

    td_menu remove parseInt on $td_render_atts['show_child_at']

--- wp_booster/td_menu.php
+++ wp_booster/td_menu.php
@@ -112,7 +112,7 @@ class td_menu {
 
                         $sub_categories_count = 0;
                         foreach ($td_subcategories as $td_category) {
-                            if ($sub_categories_count == parseInt($td_render_atts['show_child_cat'], 10)) { // only show 5 subcategories in the mobile menu - the same limit applies to the mega menu
+                            if ($sub_categories_count == $td_render_atts['show_child_cat']) { // only show 5 subcategories in the mobile menu - the same limit applies to the mega menu
                                 break;
                             }
                             $new_item = $this->generate_wp_post();
