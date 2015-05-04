df8e3d004d14626e89fe6160cf96760fcc4dfe85
tagdiv <contact@email.ro>
2015-05-04 09:53:54 +0300

    theme name changed

--- wp_booster/wp-admin/panel/views/td_panel_header.php
+++ wp_booster/wp-admin/panel/views/td_panel_header.php
@@ -379,7 +379,7 @@
 			echo td_panel_generator::input(array(
 				'ds' => 'td_option',
 				'option_id' => 'tds_logo_text',
-				'placeholder' => 'NEWSMAG'
+				'placeholder' => 'NEWSPAPER'
 			));
 			?>
 		</div>
