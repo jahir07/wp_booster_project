507aefb4779f75f870384c7694ecbf1f11bd3a88
marius <pmarius05@yahoo.com>
2015-05-15 17:29:44 +0300

    admin panel

---
+++ wp_booster/wp-admin/panel/td_view_support.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "td_view_header.php";
+?>
+<div class="td-admin-wrap">
+
+		<p>We know what it’s like to need support. This is the reason why our customers are the top priority and we treat every issue with utmost seriousness. The team is working hard to help every customer, to keep the theme’s documentation up to date, to produce video tutorials and to develop new ways to make everything easier.</p>
+		<p>You can count on us, we are here for you!</p>
+
+
+
+	<div class="td-admin-box-text td-admin-box-three">
+		<h2>Support forum</h2>
+		<p>We offer outstanding support through our forum. To get support first you need to register  (create an account) and open a thread in the Newspaper Section.</p>
+		<a class="button button-primary">Open forum</a>
+	</div>
+	<div class="td-admin-box-text td-admin-box-three">
+		<h2>Support forum</h2>
+		<p>We offer outstanding support through our forum. To get support first you need to register  (create an account) and open a thread in the Newspaper Section.</p>
+		<a class="button button-primary">Open forum</a>
+	</div>
+	<div class="td-admin-box-text td-admin-box-three">
+		<h2>Support forum</h2>
+		<p>We offer outstanding support through our forum. To get support first you need to register  (create an account) and open a thread in the Newspaper Section.</p>
+		<a class="button button-primary">Open forum</a>
+	</div>
+
+
+</div>
