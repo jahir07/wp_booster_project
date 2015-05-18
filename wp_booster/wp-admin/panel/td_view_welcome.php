b498444d46dba6e6ff5b6515d172823938caa78e
marius <pmarius05@yahoo.com>
2015-05-18 12:18:57 +0300

    admin panel

--- wp_booster/wp-admin/panel/td_view_welcome.php
+++ wp_booster/wp-admin/panel/td_view_welcome.php
@@ -6,3 +6,11 @@
 require_once "td_view_header.php";
 ?>
 
+<div class="td-admin-wrap">
+	<p>Passionate and hard working constantly looking for visual perfection, we love designing and coding WordPress themes. Continuously researching the news niche for the latest trends in design and functionality, open to new ideas, we encourage our users to provide constructive feedback.</p>
+	<p>We worked very hard and spend a great amount of time, practically thousands of hours, to create this great theme and we will do our absolute best to support it and fix all the issues you may encounter.</p>
+	<div class="td-admin-box-text"><p>Thank you for choosing the best theme we have ever build!</p>
+		<a class="button button-primary" href="http://demo.tagdiv.com/<?php echo strtolower(TD_THEME_NAME);?>" target="_blank">View demo</a>
+		<a class="button button-primary" href="http://themeforest.net/user/tagDiv" target="_blank">Our portfolio</a>
+	</div>
+</div>
