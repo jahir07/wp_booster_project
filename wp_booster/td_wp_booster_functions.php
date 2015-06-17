604f91e82e8ac14f07fc9b716caeee3ddaf6deb0
tagdiv <contact@email.ro>
2015-06-17 10:00:44 +0300

    td_wp_booster_functions.php small mod

--- wp_booster/td_wp_booster_functions.php
+++ wp_booster/td_wp_booster_functions.php
@@ -363,7 +363,40 @@ function hook_wp_head() {
 		}
         add_filter('body_class','td_hook_add_custom_body_class');
 	}
+
+	// if speed booster is active, the body animation classes are added into the head style, otherwise they are applied too late (the same is td-backstretch)
+	if (defined('TD_SPEED_BOOSTER')) {
+		echo '<style type="text/css">
+			body.td-animation-stack-type0 .td-animation-stack .entry-thumb,
+			body.td-animation-stack-type0 .post img {
+			  opacity: 0;
+			}
+
+			body.td-animation-stack-type1 .td-animation-stack .entry-thumb,
+			body.td-animation-stack-type1 .post .entry-thumb,
+			body.td-animation-stack-type1 .post img[class*="wp-image-"],
+			body.td-animation-stack-type1 .post a.td-sml-link-to-image > img {
+			  opacity: 0;
+			  transform: scale(0.95);
+			}
+
+			body.td-animation-stack-type2 .td-animation-stack .entry-thumb,
+			body.td-animation-stack-type2 .post .entry-thumb,
+			body.td-animation-stack-type2 .post img[class*="wp-image-"],
+			body.td-animation-stack-type2 .post a.td-sml-link-to-image > img {
+			  opacity: 0;
+			  .transform(translate(0px,10px));
+			}
+
+			.td-backstretch {
+			    display: block;
+			    max-width: none;
+			    opacity: 0;
+			    transition: opacity 2s ease 0s;
+			}</style>';
+	}
 }
+
 
 /** ----------------------------------------------------------------------------
  * The function hook to alter body css classes.
@@ -828,37 +861,6 @@ function add_ie_html5_shim () {
 
 
 
-/**
- * - the animation stack css body classes must be prepended into the head when the speed booster is active
- * - why? Because they are applied too late when the speed booster is active
- */
-add_action('wp_head', 'td_preload_animation_style');
-function td_preload_animation_style() {
-	if (defined('TD_SPEED_BOOSTER')) {
-		echo '<style>
-			body.td-animation-stack-type0 .td-animation-stack .entry-thumb,
-			body.td-animation-stack-type0 .post img {
-			  opacity: 0;
-			}
-
-			body.td-animation-stack-type1 .td-animation-stack .entry-thumb,
-			body.td-animation-stack-type1 .post .entry-thumb,
-			body.td-animation-stack-type1 .post img[class*="wp-image-"],
-			body.td-animation-stack-type1 .post a.td-sml-link-to-image > img {
-			  opacity: 0;
-			  transform: scale(0.95);
-			}
-
-			body.td-animation-stack-type2 .td-animation-stack .entry-thumb,
-			body.td-animation-stack-type2 .post .entry-thumb,
-			body.td-animation-stack-type2 .post img[class*="wp-image-"],
-			body.td-animation-stack-type2 .post a.td-sml-link-to-image > img {
-			  opacity: 0;
-			  .transform(translate(0px,10px));
-			}
-		</style>';
-	}
-}
 
 
 
