4cba3b281d637daf068f21a06bd980b302e8e478
racoder <ra_coder@yahoo.com>
2015-07-24 16:52:04 +0300

    woo + sidebar

---
+++ wp_booster/td_api.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @td_get_inline */
+require_once('td_api_base.php');
+require_once('td_api_block.php');
+require_once('td_api_block_template.php');
+require_once('td_api_category_template.php');
+require_once('td_api_category_top_posts_style.php');
+require_once('td_api_footer_template.php');
+require_once('td_api_header_style.php');
+require_once('td_api_module.php');
+require_once('td_api_single_template.php');
+require_once('td_api_smart_list.php');
+require_once('td_api_thumb.php');
+require_once('td_api_top_bar_template.php');
+require_once('td_api_tinymce_formats.php');
+require_once('td_api_autoload.php');
+/* @td_end_get_inline */
