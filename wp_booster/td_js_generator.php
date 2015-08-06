d76910bfd2854d15a3fde93b613a3a18e2a808a2
racoder <ra_coder@yahoo.com>
2015-08-06 12:18:22 +0300

    refactorizare blockuri

--- wp_booster/td_js_generator.php
+++ wp_booster/td_js_generator.php
@@ -67,39 +67,39 @@ function tdBlock() {
     <script>
         // td_js_generator - mini detector
         (function(){
-            var html_tag = document.getElementsByTagName("html")[0];
+            var htmlTag = document.getElementsByTagName("html")[0];
 
-            if(navigator.userAgent.indexOf("MSIE 10.0") > -1) {
-                html_tag.className+=' ie10';
+            if ( navigator.userAgent.indexOf("MSIE 10.0") > -1 ) {
+                htmlTag.className += ' ie10';
             }
 
-            if(!!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-                html_tag.className+=' ie11';
+            if ( !!navigator.userAgent.match(/Trident.*rv\:11\./) ) {
+                htmlTag.className += ' ie11';
             }
 
-            if (/(iPad|iPhone|iPod)/g.test( navigator.userAgent )) {
-                html_tag.className+=' td-md-is-ios';
+            if ( /(iPad|iPhone|iPod)/g.test( navigator.userAgent ) ) {
+                htmlTag.className += ' td-md-is-ios';
             }
 
             var user_agent = navigator.userAgent.toLowerCase();
-            if(user_agent.indexOf("android") > -1) {
-                html_tag.className+=' td-md-is-android';
+            if ( user_agent.indexOf("android") > -1 ) {
+                htmlTag.className += ' td-md-is-android';
             }
 
-            if (navigator.userAgent.indexOf('Mac OS X') != -1) {
-                html_tag.className+=' td-md-is-os-x';
+            if ( -1 !== navigator.userAgent.indexOf('Mac OS X')  ) {
+                htmlTag.className += ' td-md-is-os-x';
             }
 
-            if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())) {
-               html_tag.className+=' td-md-is-chrome';
+            if ( /chrom(e|ium)/.test(navigator.userAgent.toLowerCase()) ) {
+               htmlTag.className += ' td-md-is-chrome';
             }
 
-            if (navigator.userAgent.indexOf('Firefox') != -1) {
-                html_tag.className+=' td-md-is-firefox';
+            if ( -1 !== navigator.userAgent.indexOf('Firefox') ) {
+                htmlTag.className += ' td-md-is-firefox';
             }
 
-            if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-                html_tag.className+=' td-md-is-safari';
+            if ( -1 !== navigator.userAgent.indexOf('Safari') && -1 === navigator.userAgent.indexOf('Chrome') ) {
+                htmlTag.className += ' td-md-is-safari';
             }
 
         })();
