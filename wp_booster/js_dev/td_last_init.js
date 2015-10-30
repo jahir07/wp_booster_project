16f34df94689cedf682dd43bd647c8456d332f32
tagdiv <contact@email.ro>
2015-10-30 17:07:46 +0200

    td_last_init.js jshint

--- wp_booster/js_dev/td_last_init.js
+++ wp_booster/js_dev/td_last_init.js
@@ -23,10 +23,10 @@ jQuery( window ).ready(function() {
     jQuery( '.td_smart_list_1 a, .td_smart_list_3 a' ).click(function( event ) {
 
         if ( event.target === event.currentTarget ) {
-            var current_url = jQuery( this ).attr( 'href' );
+            var currentUrl = jQuery( this ).attr( 'href' );
 
-            if ( ( window.location.href !== current_url) && tdUtil.isValidUrl( current_url ) ) {
-                window.location.href = current_url;
+            if ( ( window.location.href !== currentUrl ) && tdUtil.isValidUrl( currentUrl ) ) {
+                window.location.href = currentUrl;
             }
         }
     });
