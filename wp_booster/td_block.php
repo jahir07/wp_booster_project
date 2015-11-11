c982c12b65611fadcbda520106471eb7e48d7f47
puiu <socaciun@yahoo.com>
2015-11-11 14:46:45 +0200

    - fixed the categories sort order for Ajax dropdown on blocks. The categories are now displayed in the order set by the user.

--- wp_booster/td_block.php
+++ wp_booster/td_block.php
@@ -76,11 +76,23 @@ class td_block {
                             'exclude' => '1',
                             'number' => 100 //limit the number of categories shown in the drop down
                         ));
-                        foreach ($td_categories as $td_category) {
-                            $td_pull_down_items []= array (
-                                'name' => $td_category->name,
-                                'id' => $td_category->cat_ID,
-                            );
+
+                        // break the categories string
+                        $td_ajax_filter_ids = explode(',', $td_ajax_filter_ids);
+
+                        // order the categories - match the order set in the block settings
+                        foreach ($td_ajax_filter_ids as $td_category_id) {
+                            foreach ($td_categories as $td_category) {
+
+                                // retrieve the category
+                                if ($td_category_id == $td_category->cat_ID) {
+                                    // temporary buffer
+                                    $td_pull_down_items [] = array(
+                                        'name' => $td_category->name,
+                                        'id' => $td_category->cat_ID,
+                                    );
+                                }
+                            }
                         }
                         break;
 
