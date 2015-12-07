12fb1276e3f18cba83b4c491b83a899b47162e4a
racoder <ra_coder@yahoo.com>
2015-12-07 10:21:06 +0200

    custom css compiler for demos

--- wp_booster/external/td_node_less/td_less_compiler.php
+++ wp_booster/external/td_node_less/td_less_compiler.php
@@ -5,52 +5,70 @@
  */
 
 
-/**
- * @param $source
- * @param $destination
- */
 
-function td_compile_less_file($source, $destination) {
-	if (file_exists($destination)) {
-		// if the file is in used, try 10 times with 1 seconds delay
-		for ( $i = 0 ; $i < 10; $i++) {
-			$unlink_status = @unlink($destination);   // this returns false if the file is in use
-			if ($unlink_status === true) {
-				break;
-			}
-			sleep(1);
-		}
-	}
-	$cmd = 'includes\wp_booster\external\td_node_less\node.exe includes\wp_booster\external\td_node_less\lessjs\bin\lessc "' . $source . '" "' . $destination . '" --no-color';
-	$descriptorspec = array(
-		0 => array("pipe", "r"), // STDIN
-		1 => array("pipe", "w"), // STDOUT
-		2 => array("pipe", "w"), // STDERR
-	);
-	$cwd = getcwd();
-	$env = null;
-	$proc = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
-	if (is_resource($proc)) {
-		$stdout = stream_get_contents($pipes[1]);
-		$stderr = stream_get_contents($pipes[2]);
-		$return_status = proc_close($proc);
+class td_less_compiler {
 
-		if ($return_status == 1) {
-			echo '<pre>' . $stderr . '</pre>';
-			die;
-		} else {
+	static $compiler_cmd = 'includes\wp_booster\external\td_node_less\node.exe includes\wp_booster\external\td_node_less\lessjs\bin\lessc';
+
+
+	static function compile_and_redirect($source, $destination) {
+		$response = self::compile_less_file($source, $destination);
+		if ($response === true) {
+			// everything worked ok
 			header('Location: ' . $destination);
 		}
+	}
+
+
+
+
+	static function compile_less_file($source, $destination) {
+		if (file_exists($destination)) {
+			// if the file is in used, try 10 times with 1 seconds delay
+			for ( $i = 0 ; $i < 10; $i++) {
+				$unlink_status = @unlink($destination);   // this returns false if the file is in use
+				if ($unlink_status === true) {
+					break;
+				}
+				sleep(1);
+			}
+		}
+
+
+		$cmd = self::$compiler_cmd . ' "' . $source . '" "' . $destination . '" --no-color';
+		$descriptorspec = array(
+			0 => array("pipe", "r"), // STDIN
+			1 => array("pipe", "w"), // STDOUT
+			2 => array("pipe", "w"), // STDERR
+		);
+		$cwd = getcwd();
+		$env = null;
+		$proc = proc_open($cmd, $descriptorspec, $pipes, $cwd, $env);
+		if (is_resource($proc)) {
+			$stdout = stream_get_contents($pipes[1]);
+			$stderr = stream_get_contents($pipes[2]);
+			$return_status = proc_close($proc);
+
+			if ($return_status == 1) {
+				// error
+				echo '<pre>' . $stderr . '</pre>';
+				die;
+			} else {
+				// everything worked ok
+				return true;
+			}
+
 
 //                // Output test:
 //                echo "STDOUT:<br />";
 //                echo "<pre>".$stdout."</pre>";
 //                echo "STDERR:<br />";
 //                echo "<pre>".$stderr."</pre>";
-
-		echo "Exited with status: $return_status";
-	} else {
-		echo 'td_error: no resource';
+//				echo "Exited with status: $return_status";
+		} else {
+			echo 'td_error: no resource';
+			die;
+		}
 	}
 }
 
@@ -58,28 +76,21 @@ function td_compile_less_file($source, $destination) {
 
 
 
-if (isset($_GET['part'])) {
-	switch ($_GET['part']) {
 
-		case 'style.css_v2':
-			td_compile_less_file('includes/less_files/main.less', 'style.css');
-			break;
 
-		case 'editor-style':
-			td_compile_less_file('includes/less_files/editor-style.less', 'editor-style.css');
-			break;
 
-		case 'woocommerce':
-			td_compile_less_file('includes/less_files/woocommerce/main.less', 'style-woocommerce.css');
-			break;
 
-		case 'wp-admin.css':
-			td_compile_less_file('includes/wp_booster/wp-admin/css/wp-admin.less', 'includes/wp_booster/wp-admin/css/wp-admin.css');
-			break;
 
-		case 'style.css_mobile':
-			td_compile_less_file('mobile/includes/less_files/main.less', 'mobile/style.css');
-			break;
 
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
