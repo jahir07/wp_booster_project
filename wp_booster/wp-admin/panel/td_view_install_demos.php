ef1ba345bd2b317a09d34a629b9303d6c45b7316
cristianpantazescu <c_pantazescu@yahoo.com>
2015-12-10 11:46:23 +0200

    New Panel Demos

--- wp_booster/wp-admin/panel/td_view_install_demos.php
+++ wp_booster/wp-admin/panel/td_view_install_demos.php
@@ -134,70 +134,97 @@ die;
 	<?php } ?>
     <hr/>
 
+    <div class="td-demo-page-content">
 
-	<div class="td-admin-columns">
-		<?php
+        <div class="td-admin-columns td-install-demos">
+            <?php
 
-        $installed_demo = td_demo_state::get_installed_demo();
-
-
+            $installed_demo = td_demo_state::get_installed_demo();
 
 
-        foreach (td_global::$demo_list as $demo_id => $stack_params) {
-            $tmp_class = '';
-            if ($installed_demo !== false and $installed_demo['demo_id'] == $demo_id) {
-                $tmp_class = 'td-demo-installed';
-            }
-            ?>
 
-			<div class="td-demo-<?php echo $demo_id ?> td-wp-admin-demo theme <?php echo $tmp_class ?>">
 
-				<!-- Import content -->
-				<div class="theme-screenshot">
-					<img class="td-demo-thumb" src="<?php echo td_global::$demo_list[$demo_id]['img'] ?>"/>
-                </div>
+            foreach (td_global::$demo_list as $demo_id => $stack_params) {
+                $tmp_class = '';
+                if ($installed_demo !== false and $installed_demo['demo_id'] == $demo_id) {
+                    $tmp_class = 'td-demo-installed';
+                }
+                ?>
 
-				<div class="td-admin-title">
-					<div class="td-progress-bar-wrap"><div class="td-progress-bar"></div></div>
-					<h3 class="theme-name"><?php echo $stack_params['text'] ?></h3>
-				</div>
+                <div class="td-demo-<?php echo $demo_id ?> td-wp-admin-demo theme <?php echo $tmp_class ?>">
 
-				<div class="td-admin-checkbox td-small-checkbox">
-                    <div class="td-demo-install-content">
-                        <?php
-                        echo td_panel_generator::checkbox(array(
-                            'ds' => 'td_import_theme_styles',
-                            'option_id' => 'td_import_menus',
-                            'true_value' => '',
-                            'false_value' => 'no'
-                        ));
-                        ?>
-                        <p>Include content</p>
+                    <!-- Import content -->
+
+                    <div class="theme-screenshot">
+                        <div class="td-progress-bar-wrap"><div class="td-progress-bar"></div></div>
+                        <img class="td-demo-thumb" src="<?php echo td_global::$demo_list[$demo_id]['img'] ?>"/>
                     </div>
-                        <p class="td-installed-text">
-	                    <?php
-	                    if (!empty(td_global::$demo_list[$demo_id]['demo_installed_text'])) {
-		                    echo td_global::$demo_list[$demo_id]['demo_installed_text'];
-	                    } else {
-		                    echo 'Demo installed!';
-	                    }
-	                    ?>
-	                    </p>
-				</div>
 
-				<div class="theme-actions">
-					<a class="button button-secondary td-button-demo-preview" href="<?php echo td_global::$demo_list[$demo_id]['demo_url'] ?>" target="_blank">Preview</a>
-					<a class="button button-secondary td-button-install-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Install</a>
-                    <a class="button button-primary td-button-uninstall-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Uninstall</a>
-                    <a class="button button-primary disabled td-button-installing-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Installing...</a>
-                    <a class="button button-secondary disabled td-button-demo-disabled" href="#"">Install</a>
+                    <div class="td-demo-meta">
+                        <div class="td-admin-title">
+                            <h3 class="theme-name"><?php echo $stack_params['text'] ?></h3>
+                        </div>
 
+                        <div class="theme-actions">
+                            <a class="button button-secondary td-button-demo-preview" href="<?php echo td_global::$demo_list[$demo_id]['demo_url'] ?>" target="_blank">Preview</a>
+                            <a class="button button-secondary td-button-install-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Install</a>
+                            <a class="button button-primary td-button-uninstall-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Uninstall</a>
+                            <a class="button button-primary disabled td-button-installing-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Installing...</a>
+                            <a class="button button-secondary disabled td-button-demo-disabled" href="#"">Install</a>
+                            <a class="button button-primary disabled td-button-uninstalling-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Uninstalling...</a>
+                        </div>
 
-                    <a class="button button-primary disabled td-button-uninstalling-demo" href="#" data-demo-id="<?php echo $demo_id ?>">Uninstalling...</a>
-				</div>
-			</div>
-		<?php } ?>
-	</div>
+                        <div class="td-admin-checkbox td-small-checkbox">
+                            <div class="td-demo-install-content">
+                                <?php
+                                echo td_panel_generator::checkbox(array(
+                                    'ds' => 'td_import_theme_styles',
+                                    'option_id' => 'td_import_menus',
+                                    'true_value' => '',
+                                    'false_value' => 'no'
+                                ));
+                                ?>
+                                <p>Include content</p>
+                            </div>
+                            <p class="td-installed-text">
+                                <?php
+                                if (!empty(td_global::$demo_list[$demo_id]['demo_installed_text'])) {
+                                    echo td_global::$demo_list[$demo_id]['demo_installed_text'];
+                                } else {
+                                    echo 'Demo installed!';
+                                }
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            <?php } ?>
+        </div>
+
+        <div class="td-demos-summary">
+            <span>Demos Summary</span>
+            <ul>
+                <li><a href="#">Default</a></li>
+                <li><a href="#">Animals News</a></li>
+                <li><a href="#">Black Version</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Cafe</a></li>
+                <li><a href="#">Cars</a></li>
+                <li><a href="#">College</a></li>
+                <li><a href="#">Fashion</a></li>
+                <li><a href="#">Health & Fitness</a></li>
+                <li><a href="#">Local News</a></li>
+                <li><a href="#">News Magazine</a></li>
+                <li><a href="#">Politics</a></li>
+                <li><a href="#">Sport News</a></li>
+                <li><a href="#">Tech News</a></li>
+                <li><a href="#">Video News</a></li>
+                <li><a href="#">Wedding News</a></li>
+            </ul>
+        </div>
+    </div>
+
 </div>
 
 
