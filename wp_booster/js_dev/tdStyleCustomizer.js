b050a44c702e8e921ad18b1c96ecc76520372689
nicugergely <nicugergely@gmail.com>
2016-01-13 20:13:43 +0200

    demo menu updated for Chrome and IE

--- wp_booster/js_dev/tdStyleCustomizer.js
+++ wp_booster/js_dev/tdStyleCustomizer.js
@@ -66,6 +66,78 @@ jQuery().ready(function() {
         }
     });
 
+    var mousePosX = 0;
+    var mousePosY = 0;
+
+    jQuery( document ).mousemove(function( event ) {
+        if ( event.pageX || event.pageY ) {
+            mousePosX = event.pageX;
+            mousePosY = event.pageY;
+        } else if (event.clientX || event.clientY ) {
+            mousePosX = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            mousePosY = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+    });
+
+
+    var checkMousePosition = function() {
+
+        var theElement;
+
+        jQuery( '.td-set-theme-style-link' ).each(function(index, element) {
+
+            var $this = jQuery(element),
+                cssClassContainer = 'td-set-theme-style',
+                $thisContainer = $this.closest( '.' + cssClassContainer );
+
+            var verticalPosition = false;
+            var horizontalPosition = false;
+
+            if ( 0 === jQuery( '.td-set-theme-style-link' ).index( element ) % 2 ) {
+
+                if ( parseInt($thisContainer.position().top) + parseInt(jQuery(window).scrollTop()) < mousePosY && mousePosY < parseInt($thisContainer.position().top) + parseInt($thisContainer.outerHeight()) ) {
+                    verticalPosition = true;
+
+                    if ( parseInt($thisContainer.position().left) < mousePosX && mousePosX < parseInt($thisContainer.position().left) + parseInt($thisContainer.outerWidth()) ) {
+                        horizontalPosition = true;
+                    }
+                }
+                //console.log( 'caz A : ' + index + ' > vert: ' + verticalPosition + ' > hori: ' + horizontalPosition + ' > posY: ' + mousePosY + ' > posX: ' + mousePosX +
+                //    ' > top: ' + (parseInt($thisContainer.position().top) + parseInt(jQuery(window).scrollTop())) + ' > bottom: ' + (parseInt($thisContainer.position().top) + parseInt($thisContainer.outerHeight())) +
+                //    ' > left: ' + parseInt($thisContainer.position().left) + ' > right: ' + (parseInt($thisContainer.position().left) + parseInt($thisContainer.outerWidth())) );
+
+            } else {
+                var $thisPrevContainer = $thisContainer.prev( '.' + cssClassContainer );
+
+                if ( $thisPrevContainer.length ) {
+                    if ( parseInt($thisPrevContainer.position().top) + parseInt(jQuery(window).scrollTop()) < mousePosY && mousePosY < (parseInt($thisPrevContainer.position().top) + parseInt($thisPrevContainer.outerHeight())) ) {
+                        verticalPosition = true;
+
+                        if ( parseInt($thisContainer.position().left) < mousePosX && mousePosX < (parseInt($thisContainer.position().left) + parseInt($thisContainer.outerWidth())) ) {
+                            horizontalPosition = true;
+                        }
+                    }
+                }
+                //console.log( 'caz B : ' + index + ' > vert: ' + verticalPosition + ' > hori: ' + horizontalPosition + ' > posY: ' + mousePosY + ' > posX: ' + mousePosX +
+                //    ' > top: ' + ($thisPrevContainer.position().top + parseInt(jQuery(window).scrollTop())) + ' > bottom: ' + (parseInt($thisPrevContainer.position().top) + parseInt($thisPrevContainer.outerHeight())) +
+                //    ' > left: ' + $thisContainer.position().left + ' > right: ' + (parseInt($thisContainer.position().left) + parseInt($thisContainer.outerWidth())) );
+            }
+
+            // The element where the mouse is positioned, was found
+            if ( verticalPosition && horizontalPosition ) {
+                theElement = element;
+                return;
+            }
+
+        });
+
+        if ( undefined !== theElement ) {
+            jQuery( theElement).mouseenter();
+        }
+    };
+
+
+
     jQuery( '.td-set-theme-style-link' ).hover(
 
         // The mouse enter event handler
@@ -84,46 +156,46 @@ jQuery().ready(function() {
             }
 
             var
-                // The css class of the container element
+            // The css class of the container element
                 cssClassContainer = 'td-set-theme-style',
 
-                // The jquery object of the current element
+            // The jquery object of the current element
                 $this = jQuery(this),
 
-                // The jquery object of the container of the current element
+            // The jquery object of the container of the current element
                 $thisContainer = $this.closest( '.' + cssClassContainer ),
 
-                // The demo previewer
+            // The demo previewer
                 jQueryDisplayEl = jQuery( '.td-screen-demo:first' ),
 
-                // The ref top value considers the existing of the wpadminbar element
+            // The ref top value considers the existing of the wpadminbar element
                 refTopValue = 0,
 
-                // The top value set to the css top setting
+            // The top value set to the css top setting
                 topValue = 0,
 
-                // The left value set to the css left setting
+            // The left value set to the css left setting
                 leftValue = 0,
 
-                // The padding value set to the css padding-left setting
+            // The padding value set to the css padding-left setting
                 paddingLeftValue = 0,
 
-                // The extra value added to the css padding-left setting and removed from the css left setting (if we need to start earlier or later - does nothing with 0 value)
+            // The extra value added to the css padding-left setting and removed from the css left setting (if we need to start earlier or later - does nothing with 0 value)
                 extraLeftValue = 0,
 
-                // The jquery wpadminbar element
+            // The jquery wpadminbar element
                 jqWPAdminBar = jQuery( '#wpadminbar' );
 
 
             jQueryDisplayEl.find( 'img:first' ).attr( 'src', $this.data( 'screen-url' ));
 
-            // The second (paired) column
+            // The first (paired - 0) column
             if ( 0 === jQuery( '.td-set-theme-style-link' ).index( this ) % 2 ) {
                 topValue = $thisContainer.position().top + $thisContainer.outerHeight() / 2 - jQueryDisplayEl.outerHeight(true) / 2;
                 leftValue = $thisContainer.outerWidth(true) - extraLeftValue;
                 paddingLeftValue = $thisContainer.outerWidth(true) + extraLeftValue;
 
-            // The first (impaired) column
+                // The second (impaired - 1) column
             } else {
                 var $thisPrevContainer = $thisContainer.prev( '.' + cssClassContainer );
 
@@ -180,85 +252,95 @@ jQuery().ready(function() {
         // The mouse exit event handler
         function( event ) {
 
-            //console.log( 'out MAIN ' + contor++);
+            //console.log('out MAIN ');
 
             var
-                // The jquery object of the previewer demo element
-                jQueryDisplayEl = jQuery( '.td-screen-demo:first' ),
+            // The jquery object of the previewer demo element
+                jQueryDisplayEl = jQuery('.td-screen-demo:first'),
 
-                // The css left value
-                existingLeftValue = jQueryDisplayEl.css( 'left' ),
+            // The css left value
+                existingLeftValue = jQueryDisplayEl.css('left'),
 
-                // The css padding-left value
-                existingExtraLeftValue = jQueryDisplayEl.css( 'padding-left' ),
+            // The css padding-left value
+                existingExtraLeftValue = jQueryDisplayEl.css('padding-left'),
 
-                // The css width value
-                existingWidthValue = jQueryDisplayEl.css( 'width' ),
+            // The css width value
+                existingWidthValue = jQueryDisplayEl.css('width'),
 
-                // The integer css left value
-                newLeftValue = parseInt( existingLeftValue.replace( 'px', '' ) ),
+            // The integer css left value
+                newLeftValue = parseInt(existingLeftValue.replace('px', '')),
 
-                // The integer css padding-left value
-                newExtraLeftValue = parseInt( existingExtraLeftValue.replace( 'px', '' ) ),
+            // The integer css padding-left value
+                newExtraLeftValue = parseInt(existingExtraLeftValue.replace('px', '')),
 
-                // The step value used to decrease the padding-left css value and to increase the left css value
+            // The step value used to decrease the padding-left css value and to increase the left css value
                 step = 10,
 
-                // The waiting time (ms) for the timeout
-                startTimeoutWait = 40,
+            // The waiting time (ms) for the timeout
+                startTimeoutWait = 50,
 
-                // The wait time (ms) for the interval
+            // The time (ms) for the interval
                 startIntervalWait = 15,
 
-                newWidthValue = parseInt( existingWidthValue.replace( 'px', '' ) );
+                newWidthValue = parseInt(existingWidthValue.replace('px', ''));
 
 
-            if ( newExtraLeftValue > 0 ) {
+            var $this = jQuery(this);
+
+
+            if (newExtraLeftValue > 0) {
 
                 // Clear any timeout if there's one, because a new one will be created
-                if ( undefined !== startTimeout ) {
-                    window.clearTimeout( startTimeout );
+                if (undefined !== startTimeout) {
+                    window.clearTimeout(startTimeout);
                 }
 
                 // Clear any interval if there's one, because a new one will be created
-                if ( undefined !== startInterval ) {
-                    window.clearInterval( startInterval );
+                if (undefined !== startInterval) {
+                    window.clearInterval(startInterval);
                 }
 
-                startTimeout = setTimeout(function() {
-                    startInterval = setInterval(function() {
+                startTimeout = setTimeout(function () {
+                    startInterval = setInterval(function () {
 
-                            if ( newExtraLeftValue <= 0 ) {
-
-                                // Clear any timeout, and we should have one, because we finished
-                                if ( undefined !== startTimeout ) {
-                                    window.clearTimeout( startTimeout );
-                                }
-
-                                // Clear any interval, and we should have one, because we finished
-                                if ( undefined !== startInterval ) {
-                                    window.clearInterval( startInterval );
-                                }
-                            }
+                            var dataWidthPreview = jQueryDisplayEl.data('width-preview');
 
                             newLeftValue += step;
                             newExtraLeftValue -= step;
                             newWidthValue -= step;
 
-                            var dataWidthPreview = jQueryDisplayEl.data( 'width-preview' );
+                            var mousePositionFound = false;
 
-                            if ( newExtraLeftValue < 0 || newWidthValue < dataWidthPreview ) {
+                            if (newExtraLeftValue <= 0 || newWidthValue < dataWidthPreview || mousePosX < newLeftValue) {
+
+                                // Clear any timeout, and we should have one, because we finished
+                                if (undefined !== startTimeout) {
+                                    window.clearTimeout(startTimeout);
+                                }
+
+                                // Clear any interval, and we should have one, because we finished
+                                if (undefined !== startInterval) {
+                                    window.clearInterval(startInterval);
+                                }
+
                                 newExtraLeftValue = 0;
-                                var dataLeftValue = jQueryDisplayEl.data( 'left-value' );
-                                newLeftValue = dataLeftValue;
+                                newLeftValue = jQueryDisplayEl.data('left-value');
                                 newWidthValue = dataWidthPreview;
+
+                                mousePositionFound = true;
                             }
 
                             jQueryDisplayEl.css({
-                                'left' : newLeftValue,
+                                'left': newLeftValue,
                                 'padding-left': newExtraLeftValue,
                                 'width': newWidthValue
                             });
+
+                            // The timeout started and the interval are stopped (The mouse was reached or the css computation is done)
+                            if ( mousePositionFound ) {
+                                checkMousePosition();
+                            }
+
                         }, startIntervalWait
                     );
                 }, startTimeoutWait);
