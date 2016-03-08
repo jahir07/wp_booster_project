4e45c666d75da1c4f571159803407670df9b2e45
cristianpantazescu <c_pantazescu@yahoo.com>
2016-03-08 09:56:54 +0200

    Woo Invoice Image - Cristi

--- wp_booster/wp-admin/panel/td_view_support.php
+++ wp_booster/wp-admin/panel/td_view_support.php
@@ -6,7 +6,7 @@ require_once "td_view_header.php";
     <h1><?php echo TD_THEME_NAME ?> support</h1>
     <div class="about-text" style="margin-bottom: 32px;">
 
-        <p>We know what it’s like to need support. This is the reason why our customers are the top priority and we treat every issue with utmost seriousness. The team is working hard to help every customer, to keep the theme’s documentation up to date, to produce video tutorials and to develop new ways to make everything easier.</p>
+        <p>We know what it’s like to need support. This is the reason why our customers are the top priority and we treat every issue with the utmost seriousness. The team is working hard to help every customer, to keep the theme’s documentation up to date, to produce video tutorials and to develop new ways to make everything easier.</p>
         <p>You can count on us, we are here for you!</p>
 
     </div>
@@ -23,7 +23,7 @@ require_once "td_view_header.php";
         </div>
         <div class="td-admin-box-text td-admin-box-three">
             <h2>Docs and learning</h2>
-            <p>Our online documentation will give you important information about the theme. This is a exceptional resource to start discovering the theme’s true potential.</p>
+            <p>Our online documentation will give you important information about the theme. This is an exceptional resource to start discovering the theme’s true potential.</p>
             <a class="button button-primary" href="<?php echo TD_THEME_DOC_URL?>" target="_blank">Open documentation</a>
         </div>
         <div class="td-admin-box-text td-admin-box-three td-admin-box-last">
