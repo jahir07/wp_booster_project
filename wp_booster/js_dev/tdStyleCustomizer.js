512456d6c1424d4a047a64fda53b3dd833ca5be2
racoder <ra_coder@yahoo.com>
2016-03-21 14:13:46 +0200

    clean up style customizer

--- wp_booster/js_dev/tdStyleCustomizer.js
+++ wp_booster/js_dev/tdStyleCustomizer.js
@@ -9,24 +9,6 @@
 /* global td_set_cookies_life:Function */
 /* global tdDetect: {} */
 
-(function() {
-
-    'use strict';
-
-    var td_current_panel_stat = td_read_site_cookie( 'td_show_panel' );
-
-    if ( 'hide' === td_current_panel_stat ) {
-
-        var jQueryObj = jQuery( '#td-theme-settings' );
-        if ( jQueryObj.length ) {
-            jQueryObj.removeClass( 'td-theme-settings-small' );
-            jQuery( '#td-theme-set-hide' ).html( 'DEMOS' );
-        }
-    } else {
-        jQuery( '#td-theme-set-hide' ).html( 'CLOSE ' );
-    }
-
-})();
 
 
 var tdDemoMenu = {
@@ -626,7 +608,31 @@ var tdDemoMenu = {
 };
 
 
+/**
+ * show the panel if the cookie is set
+ */
+(function() {
+    'use strict';
+    var td_current_panel_stat = td_read_site_cookie( 'td_show_panel' );
+    if ( 'hide' === td_current_panel_stat ) {
+        var jQueryObj = jQuery( '#td-theme-settings' );
+        if ( jQueryObj.length ) {
+            jQueryObj.removeClass( 'td-theme-settings-small' );
+            jQuery( '#td-theme-set-hide' ).html( 'DEMOS' );
+        }
+    } else {
+        jQuery( '#td-theme-set-hide' ).html( 'CLOSE');
+    }
 
+})();
+
+
+
+
+
+/*  ----------------------------------------------------------------------------
+ On load
+ */
 jQuery().ready(function() {
 
     'use strict';
@@ -636,32 +642,10 @@ jQuery().ready(function() {
         tdDemoMenu.init();
     }
 
-});
 
-
-
-
-
-
-/*  ----------------------------------------------------------------------------
-@todo REMOVE THIS WHEN THE SITE IS READY
- tagDiv live css compiler ( 2013 )
- - this script is used on our demo site to customize the theme live
- - not used on production sites
- */
-var td_current_panel_stat_old = td_read_site_cookie('td_show_panel_old');
-if (td_current_panel_stat_old == 'show' || td_current_panel_stat_old == null) {
-    jQuery('.td-theme-settings-small-old').addClass('td-theme-settings-no-transition-old');
-    jQuery('.td-theme-settings-small-old').removeClass('td-theme-settings-small-old');
-}
-
-/*  ----------------------------------------------------------------------------
- On load
- */
-jQuery().ready(function() {
 
     // Show/hide the demo menu panel
-    jQuery( '#td-theme-set-hide' ).click(function(event ){
+    jQuery( '#td-theme-set-hide' ).click(function(event) {
         event.preventDefault();
         event.stopPropagation();
 
@@ -685,32 +669,5 @@ jQuery().ready(function() {
         }
     });
 
-
-
-    //hide panel
-    jQuery("#td-theme-set-hide-old").click(function(event){
-        event.preventDefault();
-        event.stopPropagation();
-        //hide
-        td_set_cookies_life(['td_show_panel_old', 'hide', 86400000]);//86400000 is the number of milliseconds in a day
-        jQuery('#td-theme-settings-old').removeClass('td-theme-settings-no-transition-old');
-        jQuery('#td-theme-settings-old').addClass('td-theme-settings-small-old');
-
-
-        jQuery('.td-set-theme-style-link-old').removeClass('fadeInLeft');
-
-    });
-
-    //show panel
-    jQuery("#td-theme-settings-old").click(function(){
-        if (jQuery(this).hasClass('td-theme-settings-small-old')) {
-
-            jQuery('.td-set-theme-style-link-old').addClass('animated_xlong fadeInLeft');
-
-            //show full
-            td_set_cookies_life(['td_show_panel_old', 'show', 86400000]);//86400000 is the number of milliseconds in a day
-            jQuery('.td-theme-settings-small-old').removeClass('td-theme-settings-small-old');
-        }
-    });
 
 }); //end on load
