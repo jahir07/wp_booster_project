21f64c982adf8e88062136d845c57ae4cfe875b7
marius <pmarius05@yahoo.com>
2016-04-20 19:17:10 +0300

    mobile menu background

--- wp_booster/wp-admin/panel/views/td_panel_background.php
+++ wp_booster/wp-admin/panel/views/td_panel_background.php
@@ -1,10 +1,10 @@
 <!-- BACKGROUND SETTINGS -->
-<?php echo td_panel_generator::box_start('Background settings'); ?>
+<?php echo td_panel_generator::box_start('Theme background'); ?>
 
     <!-- BACKGROUND UPLOAD -->
     <div class="td-box-row">
         <div class="td-box-description">
-            <span class="td-box-title">SITE BACKGROUND</span>
+            <span class="td-box-title">THEME BACKGROUND</span>
             <p>Upload a background image, the site will automatically switch to boxed version</p>
         </div>
         <div class="td-box-control-full">
@@ -103,6 +103,92 @@
 
 
 
+<?php echo td_panel_generator::box_end();?>
 
+
+<!-- MOBILE MENU BACKGROUND -->
+<?php echo td_panel_generator::box_start('Mobile menu/search background', false); ?>
+
+<!-- BACKGROUND UPLOAD -->
+<div class="td-box-row">
+    <div class="td-box-description">
+        <span class="td-box-title">BACKGROUND IMAGE</span>
+        <p>Upload a background image</p>
+    </div>
+    <div class="td-box-control-full">
+        <?php
+        echo td_panel_generator::upload_image(array(
+            'ds' => 'td_option',
+            'option_id' => 'tds_mobile_background_image'
+        ));
+        ?>
+    </div>
+</div>
+
+<!-- Background Repeat -->
+<div class="td-box-row">
+    <div class="td-box-description">
+        <span class="td-box-title">REPEAT</span>
+        <p>How the background image will be displayed</p>
+    </div>
+    <div class="td-box-control-full">
+        <?php
+        echo td_panel_generator::radio_button_control(array(
+            'ds' => 'td_option',
+            'option_id' => 'tds_mobile_background_repeat',
+            'values' => array(
+                array('text' => 'No Repeat', 'val' => ''),
+                array('text' => 'Tile', 'val' => 'repeat'),
+                array('text' => 'Tile Horizontally', 'val' => 'repeat-x'),
+                array('text' => 'Tile Vertically', 'val' => 'repeat-y')
+            )
+        ));
+        ?>
+    </div>
+</div>
+
+<!-- Background Size -->
+<div class="td-box-row">
+    <div class="td-box-description">
+        <span class="td-box-title">SIZE</span>
+        <p>Set the background image size</p>
+    </div>
+    <div class="td-box-control-full">
+        <?php
+        echo td_panel_generator::radio_button_control(array(
+            'ds' => 'td_option',
+            'option_id' => 'tds_mobile_background_size',
+            'values' => array(
+                array('text' => 'Cover', 'val' => ''),
+                array('text' => 'Full Width', 'val' => '100% auto'),
+                array('text' => 'Full Height', 'val' => 'auto 100%'),
+                array('text' => 'Auto', 'val' => 'auto'),
+                array('text' => 'Contain', 'val' => 'contain')
+            )
+        ));
+        ?>
+    </div>
+</div>
+
+<!-- Background position -->
+<div class="td-box-row">
+    <div class="td-box-description">
+        <span class="td-box-title">POSITION</span>
+        <p>Position your background image</p>
+    </div>
+    <div class="td-box-control-full">
+        <?php
+        echo td_panel_generator::radio_button_control(array(
+            'ds' => 'td_option',
+            'option_id' => 'tds_mobile_background_position',
+            'values' => array(
+                array('text' => 'Bottom', 'val' => ''),
+                array('text' => 'Center', 'val' => 'center center'),
+                array('text' => 'Top', 'val' => 'center top')
+            )
+        ));
+        ?>
+    </div>
+</div>
 
 <?php echo td_panel_generator::box_end();?>
